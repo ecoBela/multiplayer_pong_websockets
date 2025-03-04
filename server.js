d489d00ce0383c78fbf93ec4ad3a17a51aa55df3
Bela Hamid <belahamid@gmail.com>
2025-03-04 13:22:18 +0000

    Create server using socket.io

---
+++ server.js
@@ -0,0 +1,8 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server);
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
+});
